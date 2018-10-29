4d2ea8280703f20a6cbeea006622e3ca682193af
Bogdan Oancea <bogdan@Bogdans-MacBook-Air.local>
2018-10-29 18:59:48 +0200

    new tests

--- inst/tests/test_toc.R
+++ inst/tests/test_toc.R
@@ -5,6 +5,11 @@ test_that("tempo_toc", {
   
   expect_is(toc, "data.frame")
   expect_type(toc, "list")
+  expect_length(toc, 2)
+  expect_type(toc[1], "list")
+  expect_type(toc[2], "list")
+  expect_type(toc[[1]], "character")
+  expect_type(toc[[2]], "character")
   expect_named(toc)
   expect_identical(names(toc)[1], "Denumire")
   expect_identical(names(toc)[2], "Cod")
