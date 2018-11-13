3ed08dde0ccd9e90636c9de478bd1d4807d66d8d
Ana Tiru <tiru.ana21@gmail.com>
2018-11-13 19:22:04 +0200

    add simplified parameter; add roxygen comments

--- R/tempo_last_up.R
+++ R/tempo_last_up.R
@@ -1,15 +1,18 @@
 #' @title TEMPO Last Updates 
 #'
-#' @description tempo_last_up 
-#' The function returns a dataframe containing the last updates operated on TEMPO tables.
+#' @description 
+#' Returns a dataframe containing the last updates operated on TEMPO tables.
 #' 
-#' @param code
+#' @param code  String representing the code for
+#' the table/matrix in TEMPO Online database. See more on
+#' how to obtain a matrix code from \code{\link{tempo_toc}}.
 #' 
-#' @param simplified
+#' @param simplified  Logical indicating whether results 
+#' should be simplified. The default value is FALSE.
 #' 
-#' @param language - a string to set the language for the downloaded
+#' @param language  String to set the language for the downloaded
 #' tables. Options: "ro" - for Romanian and "en" - for English. If no parameter
-#' is given, implicitly downloads tables in Romanian
+#' is given, implicitly downloads tables in Romanian.
 #' 
 #' @return Returns a dataframe object. 
 #' 
@@ -24,9 +27,6 @@
 #' @import rvest
 #' @export
 
-#TODO
-# parameter simplified; complete roxygen comments
-
 tempo_last_up <- function (code = NULL, simplified = FALSE, language = c("ro", "en")) {
   if (language[1] == "ro") {
     update <- read_html("http://statistici.insse.ro:8077/tempo-ins/news/")
@@ -36,12 +36,16 @@ tempo_last_up <- function (code = NULL, simplified = FALSE, language = c("ro", "
     cat("Invalid argument for language: ", language[1], "\nArguments accepted: \"ro\" or \"en\".\n")
     return (NULL)
   }
-
+  
   update <- update %>% html_node("table") %>% html_table(header = TRUE, fill = TRUE)
   
   if (!is.null(code)) {
     index <- which(!is.na(match(update[,4], code)))
     update <- update[index,]
+  }
+  
+  if (simplified == TRUE) {
+    update <- update[,c(2,4,5)]
   }
   
   return(update)
