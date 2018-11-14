54fee998957c81ef3c7491837d56e78a36728b1c
Ana Maria Antoaneta Tiru <Ana.Tiru@insse.ro>
2018-11-14 16:03:34 +0200

    add .Rd files

--- R/tempo_search.R
+++ R/tempo_search.R
@@ -1,14 +1,14 @@
 #' @title Get TOC for Tempo Online database filtered by keyword
 #'
-#' @description tempo_search
-#' The function searches for datasets codes in TOC based on specified keywords, 
+#' @description 
+#' Searches for datasets codes in TOC based on specified keywords, 
 #' to be used with tempo_bulk function. 
 #' 
-#' @param keyword - a character vector with multiple keywords to be matched 
+#' @param keyword  Character vector with multiple keywords to be matched. 
 #'
-#' @param language - a string to set the language for the downloaded
+#' @param language  String to set the language for the downloaded
 #' tables. Options: "ro" - for Romanian and "en" - for English. If no parameter
-#' is given, implicitly downloads tables in Romanian
+#' is given, implicitly downloads tables in Romanian.
 #' 
 #' @return Returns a dataframe object. 
 #' 
@@ -17,17 +17,12 @@
 #' E.g. all TOC entries related to education.
 #' 
 #' @examples \dontrun{
-#' 
 #' tempo_search(c("education", "industry"), language = "en")
-#' 
 #' }
 #' @import curl
 #' @import jsonlite
 #' @export
 
-
-#TODO
-#add the language parameter for tempo_toc function
 tempo_search <- function(keyword = c(), language = c("ro", "en")){
   toc <- tempo_toc(full_description = TRUE, language = language)
   vchar <- apply(toc, 1 ,paste, collapse = " ")
