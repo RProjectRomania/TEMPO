8c17a73bee4ba002623ba7ea68899ad039abac6d
Ana Tiru <tiru.ana21@gmail.com>
2019-02-03 21:30:22 +0200

    tempo_plot

--- R/tempo_plot.R
+++ R/tempo_plot.R
@@ -1,103 +1,126 @@
+#' @title Visualize Geospatial Data
+#' 
+#' @description tempo_plot creates a map for visualize Geospatial Data 
+#' downloaded from TEMPO Online database
+#' 
+#' @param matrix - the table/matrix downloaded from TEMPO Online database
+#' containing Geospatial Data
+#' 
+#' @param year - a string containing the year used to plot Geospatial Data
+#' 
+#' @param area - a string to set the type of area (counties, regions, macroregions)
+#' used to group Geospatial Data
+#' 
+#' @return Returns a ggplot object. 
+#' 
+#' @examples 
+#' tempo_plot(matrix=AGR111A, year="2000", area="counties")
+#' 
+#' @import dplyr
+#' @import ggplot2
+#' @export
 
-# library(rgdal)
-# library(ggplot2)
-# library(stringi)
-# library(dplyr)
+tempo_geo_reg <- function(matrix, year, area) {
+  matrix <- subset_default(matrix)
+  
+  # year parameter
+  column_names <- names(matrix)
 
-tempo_geo <- function(matrix){
-  load("data/shapefile.rda")
+  pos_column_year <- which(names(matrix)=="Ani" | names(matrix) == "Year")
+  lv <- levels(matrix[,pos_column_year])
+  pos_year <- grep(year, lv)
   
-  shapefile@data$id <- rownames(shapefile@data)
-  sf_df <- fortify(shapefile, region = "id") # convert from sf object in dataframe object
-  df_long_lat <- merge(sf_df, shapefile@data, by = "id")
-  df_long_lat <- df_long_lat[,c(2,3,8,9,10,11,12,13)]
-  df_long_lat$long <- df_long_lat$long/10000
-  df_long_lat$lat <- df_long_lat$lat/10000
-  
-  
-  
-  col <- names(matrix)
-  idx <- grep("judete", col)
-  
-  # Parametrul an
-  # tc = tempo_clean(matrix)
-  # lv = levels(tc$Ani)
-  # pos = grep(an, lv)
-  # if(length(pos) < 1){
-  #   cat("No data available for the specified time! Data available for: ", lv, "\n")
-  #   return (NULL)
-  # }
-  # 
-  # tc = subset(tc, Ani == an)
-  
-  # Parametrul zona
-  
-  
-  if(length(idx) > 0){
-    matrix$jud <- matrix[,idx[1]]
-    matrix <- matrix[,-idx[1]]
+  if(length(pos_year) < 1) {
+    cat("No data available for the specified time! Data only available for: ", lv, "\n")
+    return (NULL)
   }
+
+  matrix <- subset(matrix, matrix[,pos_column_year] == year)
   
-  title <- ""
-  for(i in 1:(ncol(matrix)-3)){
-    lvs <- levels(matrix[,i])
-    if(length(lvs)>1){
-      lv <- lvs[2]
-      title <- paste(title, lv)
-      matrix <- subset(matrix, matrix[,i] == lv)
+  
+  # area parameter
+  pos_column_jud <- grep("(judete)", tolower(column_names))
+  pos_column_reg <- grep("(regiuni)", tolower(column_names)) 
+  pos_column_macroreg <- grep("(macroregiuni)", tolower(column_names)) 
+  pos_column_val <- grep("(valoare)", tolower(column_names))
+  pos_column_loc <- which(names(matrix)=="Localitati")
+  
+  #load("data/df_coordinates.rda")
+  
+  if(area == "counties") {
+    if(length(pos_column_jud)==0) {
+      print("No data available for jud")
+      return(NULL)
     }
+    if(length(pos_column_loc)>0){
+      matrix <- subset(matrix, trimws(as.character(matrix[,pos_column_loc]))=="TOTAL")
+    }
+    print(nrow(matrix))
+    rows_to_remove <- grep("(regiunea|macroregiunea|total)", tolower(matrix[,pos_column_jud]))
+    matrix <- matrix[-rows_to_remove,]
+    label <- aggregate(cbind(long, lat) ~ mnemonic, data=df_coordinates, FUN=function(x)mean(range(x)))
+    matrix[,pos_column_jud] <- as.character(matrix[,pos_column_jud])
+    title <- paste(unname(as.matrix(matrix[1,-c(pos_column_jud, pos_column_val, pos_column_year)])), collapse = "")
+    df_coordinates <- left_join(df_coordinates, matrix[,c(pos_column_jud, pos_column_val)], by = c("county"=column_names[pos_column_jud]))
   }
   
-  reg <- grep("regiunea", tolower(matrix$jud))
-  matrix <- matrix[-reg, ]
-  df_long_lat$name <- stri_trim(as.character(df_long_lat$name))
-  matrix$jud <- stri_trim(as.character(matrix$jud))
-  join <- left_join(df_long_lat, matrix[,c(5,6)], by = c("name" = "jud"))
+  if(area == "regions") {
+    if(length(pos_column_reg)==0) {
+      print("No data available for regions!")
+      return(NULL)
+    }
+    set_reg <- grep("^regiunea", tolower(trimws(as.character(matrix[,pos_column_reg]))))
+    
+    matrix <- matrix[set_reg,]
+    label <- aggregate(cbind(long, lat) ~ region, data=df_coordinates, FUN=function(x)mean(range(x)))
+    matrix[,pos_column_reg] <- trimws(as.character(matrix[,pos_column_reg]))
+    matrix[,pos_column_macroreg] <- gsub(" - ","-", matrix[,pos_column_macroreg])
+    title <- paste(unname(as.matrix(matrix[1,-c(pos_column_reg, pos_column_val, pos_column_year)])), collapse = "")
+    df_coordinates <- left_join(df_coordinates, matrix[,c(pos_column_reg, pos_column_val)], by = c("region"=column_names[pos_column_reg]))
+  }
   
-  val <- aggregate(cbind(long, lat) ~ Valoare, data=join, FUN=mean)
-  val$long = val$long - 1
-  val$lat = val$lat - 2.5
+  if(area == "macroregions") {
+    if(length(pos_column_macroreg)==0) {
+      print("No data available for macroregions!")
+      return(NULL)
+    }
+    set_reg <- grep("(macroregiunea)", tolower(matrix[,pos_column_macroreg]))
+    matrix <- matrix[set_reg,]
+    label <- aggregate(cbind(long, lat) ~ macroregion, data=df_coordinates, FUN=mean)
+    matrix[,pos_column_macroreg] <- trimws(as.character(matrix[,pos_column_macroreg]))
+    
+    title <- paste(unname(as.matrix(matrix[1,-c(pos_column_reg, pos_column_val, pos_column_year)])), collapse = "")
+    df_coordinates <- left_join(df_coordinates, matrix[,c(pos_column_macroreg, pos_column_val)], by = c("macroregion"=column_names[pos_column_macroreg]))
+  }
   
-  
-  plot <- ggplot(join) +  
+  title <- paste(title, "Anul", year)
+  options(scipen=999)
+  plot <- ggplot(df_coordinates) +  
     theme_bw() + 
-    geom_polygon(aes(long, lat, group=mnemonic, fill=join$Valoare, colour=""), colour = "white") +
-    geom_text(data=cnames, aes(long, lat, label=mnemonic), size=4, vjust=0) +
-    geom_text(data=val, aes(long, lat, label=Valoare), size=4, vjust=0) +
-    scale_fill_gradient(low='white', high='red', na.value = 'black')+
+    geom_polygon(aes(long, lat, group=mnemonic, fill=df_coordinates[,ncol(df_coordinates)]), colour = "white") +
+    geom_text(data=label, aes(long, lat, label=label[,1]), size=3, vjust=0) +
+    scale_fill_gradient(low='white', high='red', name = column_names[pos_column_val])+
     ggtitle(title) +
-    labs(fill="Valoare")
+    coord_fixed(1)
   
   return(plot)
 }
 
-#Testare
-tempo_bulk("AGR111A")
-tempo_bulk("AGR112B")
-AGR111A = read.csv("AGR111A.csv")
-AGR112B = read.csv("AGR112B.csv")
+subset_default <- function(matrix) {
+  matrix <- tempo_clean(matrix)
+  column_names <- names(matrix)
+  pos <- grep(("regiuni|judete|macroregiuni|ani$|localitati|valoare"), tolower(column_names))
+  
+  n = seq(ncol(matrix)) 
+  n = n[-pos]
+  
+  for(i in n) {
+    lvs <- levels(matrix[,i])
+    if(length(lvs)>1){
+      lv <- lvs[2]
+      matrix <- subset(matrix, matrix[,i] == lv)
+    }
+  }
+  return(matrix)
+}
 
-tempo_geo(AGR111A)
-
-tc = tempo_clean(AGR111A, "AGR111A")
-
-tj = tempo_search("judete")
-tr = tempo_search("regiuni")
-tm = tempo_search("macroregiuni")
-
-hard = df_coordinates %>% group_by(name) %>% summarise(n = n())
-h = AGR111A %>% group_by(Macroregiuni..regiuni.de.dezvoltare.si.judete) %>% summarise(n = n())
-i = grep("Regiunea", h$Macroregiuni..regiuni.de.dezvoltare.si.judete)
-im = grep("MACROREGIUNEA", h$Macroregiuni..regiuni.de.dezvoltare.si.judete)
-h = h[-i,]
-h = h[-im,]
-h = h[-c(38),]
-
-h = read.csv("C:/Users/ana.tiru/Desktop/jud.csv")
-c = df_coordinates %>% group_by(name, countyCode) %>% summarise(n = n())
-df_coordinates = left_join(df_coordinates, tdf[,c(2,7)], by = c("countyCode"="cod"))
-dfreg = data.frame(codr=c(6,7,1,2,3,8,4,5), reg = c("Regiunea NORD-VEST", "Regiunea CENTRU", "Regiunea NORD-EST", 
-                                                    "Regiunea SUD-EST", "Regiunea SUD-MUNTENIA", "Regiunea BUCURESTI-ILFOV",
-                                                    "Regiunea SUD-VEST OLTENIA", "Regiunea VEST"))
-df_coordinates = left_join(df_coordinates, dfreg, by = c("regionId"="codr"))
-sa = sapply(matrix$jud, grep, df_coordinates$jud)
